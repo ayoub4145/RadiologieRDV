90b704b8e28b391b1d008cc3d879de3751c6c0ac
Ayoub <ayoub.ber99@gmail.com>
2025-07-08 15:24:56 +0100

    lien google authenticate

--- radio-app/resources/views/auth/2fa/setup.blade.php
+++ radio-app/resources/views/auth/2fa/setup.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <h2>Configuration 2FA</h2>
 
-    <p>Scannez ce QR code avec l'application Google Authenticator :</p>
+    <p>Scannez ce QR code avec l'application <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=fr&pli=1" title="Installer Google Authenticate ou autre OTP app pour générer le 2FA code" target="blank" style="font-size: 18pt;color:rgb(42, 35, 35)" >Google Authenticator</a> :</p>
 
     <div>{!! $qrCode !!}</div>
 
